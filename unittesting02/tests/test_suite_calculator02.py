e5c16dcce495587d2daf55782ce7850a81a3f22e
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2023-04-23 13:17:38 -0300

    add: todos for a test suite creation

---
+++ unittesting02/tests/test_suite_calculator02.py
@@ -0,0 +1,23 @@
+"""
+    Test suite >>>>
+        A test suite is a collection of test cases, test suites, or both.
+        It is used to aggregate tests that should be executed together.
+    Test runner >>>>
+        A test runner is a component which orchestrates the execution of tests and
+        provides the outcome to the user. The runner may use a graphical interface,
+        a textual interface, or return a special value to indicate the results of
+        executing the tests.
+"""
+
+# 1. Import unittest & the required module(s)
+
+# 2. Create an instance of the TestLoader
+
+# 3. Create an instance of TestSuite
+
+# 4. Add the test cases to the test suite instance
+# >> Load the test cases from the test module(s)
+
+# 5. Create an instance of the TextTestRunner
+
+# 6. Run the TextTestRunner instance
